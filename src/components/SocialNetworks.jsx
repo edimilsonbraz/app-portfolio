fd0d51c4e4913b4a181003e344b896019bc8d8e9
edimilsonbraz <edimilson.gt8@gmail.com>
2022-08-25 15:44:29 -0300

    :sparkles: Update Links Social Midias

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,16 +3,16 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa';
 import '../styles/components/socialNetworks.sass';
 
 const socialNetworks = [
-  {name: "linkedin", icon: <FaLinkedinIn />},
-  {name: "github", icon: <FaGithub />},
-  {name: "instagram", icon: <FaInstagram />},
+  {name: "linkedin", icon: <FaLinkedinIn />, url: "https://www.linkedin.com/in/edimilsonbraz/"},
+  {name: "github", icon: <FaGithub />, url: "https://github.com/edimilsonbraz"},
+  {name: "instagram", icon: <FaInstagram />, url: "https://github.com/edimilsonbraz"},
 ];
 
 const SocialNetworks = () => {
   return (
     <section id="social-networks">
       {socialNetworks.map((network) => (
-        <a href="#" className='social-btn' id={network.name} key={network.name}>
+        <a href={network.url} className='social-btn' id={network.name} key={network.name}>
           {network.icon}
         </a>
       ))}
