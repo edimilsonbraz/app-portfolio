0d4996543f637e8860ff914e2f8be3794debc282
edimilson braz <edimilson.gt8@gmail.com>
2023-07-27 14:36:32 -0300

    :sparkles: Add project card

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,12 +1,51 @@
+import '../styles/components/projectsContainer.sass'
+
 const Projects = () => {
-  return ( 
+  return (
     <section className="projects-container">
       <h2>Projetos</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eveniet aspernatur atque? Voluptatibus ex similique sunt non aperiam, perferendis harum qui quia itaque asperiores blanditiis totam consequuntur rem quidem vel.</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        eveniet aspernatur atque? Voluptatibus ex similique sunt non aperiam,
+        perferendis harum qui quia itaque asperiores blanditiis totam
+        consequuntur rem quidem vel.
+      </p>
 
-      <a href="https://github.com/edimilsonbraz" className="btn" target="_blank">Ver Projetos</a>
+      <div className="projects-grid">
+        <div className="project-card">
+          <a href="https://fullstack-trips.vercel.app/" target='_blank'>
+            <img src="./assets/digitalbook_003.png" alt="" />
+            <div className="project-card-text">
+              <h3>Digital Book</h3>
+              <p>Sistema de reserva de viagem.</p>
+            </div>
+          </a>
+        </div>
+        <div className="project-card">
+          <img src="./assets/digitalbook_003.png" alt="" />
+          <div className="project-card-text">
+            <h3>Digital Book</h3>
+            <p>Sistema de reserva de viagem.</p>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src="./assets/digitalbook_003.png" alt="" />
+          <div className="project-card-text">
+            <h3>Digital Book</h3>
+            <p>Sistema de reserva de viagem.</p>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src="./assets/digitalbook_003.png" alt="" />
+          <div className="project-card-text">
+            <h3>Digital Book</h3>
+            <p>Sistema de reserva de viagem.</p>
+          </div>
+        </div>
+        
+      </div>
     </section>
-   );
+  )
 }
- 
-export default Projects;
+
+export default Projects
