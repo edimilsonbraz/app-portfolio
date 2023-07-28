99a31fd6b73cbce435e55c15fa77826063394955
edimilson braz <edimilson.gt8@gmail.com>
2023-07-28 09:40:30 -0300

    :sparkles: Add Project coffee delivery

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -14,10 +14,19 @@ const Projects = () => {
       <div className="projects-grid">
         <div className="project-card">
           <a href="https://fullstack-trips.vercel.app/" target='_blank'>
-            <img src="./assets/digitalbook_003.png" alt="" />
+            <img src="./assets/digitalbook_003.png" alt="digitalbook_003" />
             <div className="project-card-text">
               <h3>Digital Book</h3>
               <p>Sistema de reserva de viagem.</p>
+            </div>
+          </a>
+        </div>
+        <div className="project-card">
+          <a href="https://coffee-delivery-edimilsonbraz.vercel.app/" target='_blank'>
+            <img src="./assets/coffee-delivery.png" alt="coffee-delivery" />
+            <div className="project-card-text">
+              <h3>Coffee Delivery</h3>
+              <p>App para gerenciar um carrinho de compras de uma cafeteria.</p>
             </div>
           </a>
         </div>
@@ -35,13 +44,7 @@ const Projects = () => {
             <p>Sistema de reserva de viagem.</p>
           </div>
         </div>
-        <div className="project-card">
-          <img src="./assets/digitalbook_003.png" alt="" />
-          <div className="project-card-text">
-            <h3>Digital Book</h3>
-            <p>Sistema de reserva de viagem.</p>
-          </div>
-        </div>
+        
         
       </div>
     </section>
